bf06cde61ddc084d04de9c7be071c125673bc701
Rob Carney <robert.edward.carney@gmail.com>
2019-01-17 19:21:52 -0500

    Renaming image file

--- src/image_utils.py
+++ src/image_utils.py
@@ -2,27 +2,36 @@ import cv2
 import numpy as np
 from enum import Enum
 
+class ImageDataFormat(Enum):
+    """ImageDataFormat is an enum for specifying the format of image data you want.
+    """
+    DEFAULT = 1
+    NORMALIZED = 2
+    FLATTENED = 3
+    FLATTENED_NORMALIZED = 4
+
+
 class Image:
     """Image represents an image as a numpy array of data.
     """
-    def __init__(self, image_path)  {
+    def __init__(self, image_path):
         """Default Image constructor
 
         Args:
             image_path: file path for the image
         """
         img = cv2.imread(image_path)
+        if not img.any():
+            print("Something went wrong when reading the image. Please ensure the file location is correct")
         self.data = img
-    }
 
-    def shape(self)  {
+    def shape(self):
         """shape retrieves the shape of the Image.
 
         Returns:
             Shape of the Image as a tuple.
         """
         return self.data.shape
-    }
 
     def raw_data(self, data_format=ImageDataFormat.DEFAULT):
         """raw_data returns the data for the Image, formatted as specified.
@@ -41,13 +50,7 @@ class Image:
 
 
 
-class ImageDataFormat(Enum):
-    """ImageDataFormat is an enum for specifying the format of image data you want.
-    """
-    DEFAULT = 1
-    NORMALIZED = 2
-    FLATTENED = 3
-    FLATTENED_NORMALIZED = 4
+
 
 
 
