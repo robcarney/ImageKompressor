2be1ce5ae5f32ea4b6c4348bfe475563a5ec8a4c
Rob Carney <robert.edward.carney@gmail.com>
2019-01-17 20:14:14 -0500

    Adding argument parsing to main file

--- src/main.py
+++ src/main.py
@@ -1,11 +1,25 @@
+import argparse
+
 import image
-
-
+import k_means_compressor as kmcomp
 
 
 def main():
+    # Parse command line arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--img', default='../images/bird_uncompressed.png', dest='img_file',
+        help='File location for image to be compressed (default is ../images/bird_uncompressed.png)')
+    parser.add_argument('--num-centroids', default=20, type=int, dest='num_centroids',
+        help='The number of centroids to use in the K-Means clustering algorithm (default is 20)')
+    parser.add_argument('--iters', default=50, type=int, dest='iters',
+        help='The number of iterations to run the K-Means clustering algorithm (default is 50')
+    args = parser.parse_args()
+    img_file = args.img_file
+    num_centroids = args.num_centroids
+    iters = args.iters
+
     img = image.Image('../images/bird_uncompressed.png')
-    print(img.shape())
+    compressed_img = kmcomp.compress_image(img, num_centroids, iters)
 
 
 if __name__ == '__main__':
